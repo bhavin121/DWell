3d6c29613c297d464989b5bd25fede595b9f5d99
bhavin121 <bhavin7014316242@gmail.com>
2021-10-03 12:22:27 +0530

    DWell

--- app/src/main/java/com/code_n_droid/dwell/AddressAdapter.java
+++ app/src/main/java/com/code_n_droid/dwell/AddressAdapter.java
@@ -10,43 +10,37 @@ import java.util.List;
 
 public class AddressAdapter extends RecyclerView.Adapter<AddressVH> {
 
-    public List<String> data;
+    public Data data;
 
     @NonNull
     @Override
     public AddressVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.address_card, parent, false);
-        return new AddressVH(view).link(this);
+        return new AddressVH(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AddressVH holder, int position) {
-
+        CustomerDetail customerDetail = data.getCustomerDetails().get(position);
+        holder.binding.name.setText( customerDetail.getCustomerName() );
+        holder.binding.latLong.setText(
+                "Latitude: "+customerDetail.getCustomerAddress().getLatLong().getLatitude()+
+                        "\nLongitude: "+customerDetail.getCustomerAddress().getLatLong().getLongitude()
+        );
     }
 
     @Override
     public int getItemCount() {
-        return data.size();
+        return data.getCustomerDetails().size();
     }
 }
 
 class AddressVH extends RecyclerView.ViewHolder{
 
     AddressCardBinding binding;
-    private AddressAdapter adapter;
 
     public AddressVH(@NonNull View itemView) {
         super(itemView);
         binding = AddressCardBinding.bind(itemView);
-
-        binding.delete.setOnClickListener(view -> {
-            adapter.data.remove(getAdapterPosition());
-            adapter.notifyItemRemoved(getAdapterPosition());
-        });
-    }
-
-    public AddressVH link(AddressAdapter adapter){
-        this.adapter = adapter;
-        return this;
     }
 }
